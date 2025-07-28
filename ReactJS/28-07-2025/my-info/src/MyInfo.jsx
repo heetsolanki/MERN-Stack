7cd2752b994592d765b7e53803c0afb375946dcd
Heet Solanki <heetsolanki2006@gmail.com>
2025-07-28 18:50:17 +0530

    Create my-info

---
+++ ReactJS/28-07-2025/my-info/src/MyInfo.jsx
@@ -0,0 +1,15 @@
+import React  from "react";
+import ReactDOM from 'react-dom';
+
+function MyInfo() {
+    return <div>
+        <h1>Heet Solanki</h1>
+        <ol>
+            <li>Venice</li>
+            <li>Leh</li>
+            <li>Ladakh</li>
+        </ol>
+    </div>
+}
+
+export default MyInfo;
---
+++ ReactJS/28-07-2025/my-info/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import MyInfo from './MyInfo';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <MyInfo />
+);
+
