3c178d320759d239e73b4a39d57ab28f60993cdd
Heet Solanki <heetsolanki2006@gmail.com>
2025-09-09 21:26:14 +0530

    Create read-file.js

---
+++ September/09-09-2025/read-file.js
@@ -0,0 +1,10 @@
+import { readFile } from 'fs';
+
+readFile("file.txt", "utf8", (err, data) => {
+    if(err)
+        console.log(err);
+    else
+        console.log(data);
+});
+
+console.log("Execution Completed!")
